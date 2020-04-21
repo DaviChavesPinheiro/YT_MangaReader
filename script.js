7406c633929433e838cc04d984a154027a0ab508
Davi <davicpinheiro123@hotmail.com>
2020-04-21 13:00:24 -0300

    Created Canvas, topToolBar, bottonToolBar, next and previous page, numberPageView, close button...

--- script.js
+++ script.js
@@ -1,31 +1,216 @@
 window.onload = () => {
-    createReadElement()
-}
+  const FPS = 4;
+  const PAGE_SCALE = 0.8
+  let currentPage = 0;
 
-let hasMangaAlreadyBeenOpened = false
-const readElement = document.createElement("button")
+  let video;
 
-function createReadElement(){
-    readElement.id = "read"
-    readElement.classList.add("mm")
-    readElement.classList.add("button")
-    // readElement.classList.add("undecoded")
-    readElement.textContent = "READ"
-    readElement.addEventListener("click", openManga)
-    document.querySelector(".ytp-chrome-controls .ytp-right-controls").prepend(readElement)
-}
+  let hasMangaAlreadyBeenOpened = false;
+  const readElement = document.createElement("button");
+  const panelElement = document.createElement("div");
+  const toolBarElement = document.createElement("div");
+  const closeElement = document.createElement("button");
+  const bottonToolBarElement = document.createElement("div");
+  const pageViewer = document.createElement("div");
 
-function openManga() {
-    console.log("OK")
-    // const canvas = document.querySelector("#canvas")
+  createReadElement();
+  createPanelElement();
+  createToolBarElement();
+  createCloseElement();
+  createBottonToolBarElement();
 
-    // if (decodeElement.classList.contains("undecoded")) { //Undecoded ------> Loading
-    //     setLoadingState(canvas)
-    // } else if(decodeElement.classList.contains("loading")){//Please, wait.
-    //     decodeElement.textContent = "DECODING... Wait"
-    // } else if(decodeElement.classList.contains("decoded")){//Decoded ------> Undecoded
-    //     setUndecodedState(canvas)
-    // }
-    // isDecoded = true
-    hasMangaAlreadyBeenOpened = true
-}
+  function createReadElement() {
+    readElement.id = "read";
+    readElement.classList.add("mm");
+    readElement.classList.add("button");
+    readElement.textContent = "READ";
+    readElement.addEventListener("click", read);
+    document
+      .querySelector(".ytp-chrome-controls .ytp-right-controls")
+      .prepend(readElement);
+  }
+  function createPanelElement() {
+    panelElement.id = "panel";
+    panelElement.classList.add("mm");
+    panelElement.classList.add("panel");
+    panelElement.classList.add("hidden");
+    document.querySelector("body").append(panelElement);
+  }
+  function createToolBarElement() {
+    toolBarElement.id = "toolBar";
+    toolBarElement.classList.add("mm");
+    document.querySelector("#panel").prepend(toolBarElement);
+  }
+  function createCloseElement() {
+    closeElement.id = "close";
+    closeElement.classList.add("mm");
+    closeElement.classList.add("button");
+    closeElement.textContent = "X";
+    closeElement.addEventListener("click", read);
+    document.querySelector("#toolBar").prepend(closeElement);
+  }
+  function createBottonToolBarElement() {
+    bottonToolBarElement.id = "bottonToolBar";
+    bottonToolBarElement.classList.add("mm");
+    document.querySelector("#panel").prepend(bottonToolBarElement);
+
+    // Next Page
+    const nextPageElement = document.createElement("button");
+    nextPageElement.id = "next";
+    nextPageElement.classList.add("mm");
+    nextPageElement.classList.add("button");
+    nextPageElement.classList.add("toolBarButtons");
+    nextPageElement.textContent = "Next";
+    document.querySelector("#bottonToolBar").prepend(nextPageElement);
+    //Page Viewer
+    pageViewer.id = "pageViewer";
+    pageViewer.classList.add("mm");
+    pageViewer.classList.add("toolBarButtons");
+    pageViewer.textContent = "Page 0";
+    document.querySelector("#bottonToolBar").prepend(pageViewer);
+    // Previous Page
+    const previousPageElement = document.createElement("button");
+    previousPageElement.id = "previous";
+    previousPageElement.classList.add("mm");
+    previousPageElement.classList.add("button");
+    previousPageElement.classList.add("toolBarButtons");
+    previousPageElement.textContent = "Previous";
+    document.querySelector("#bottonToolBar").prepend(previousPageElement);
+  }
+
+  function read() {
+    if (!hasMangaAlreadyBeenOpened) {
+      openManga();
+    }
+    panelElement.classList.toggle("hidden");
+    hasMangaAlreadyBeenOpened = true;
+  }
+
+  function openManga() {
+    let intervalID;
+
+    video = document.querySelector("video");
+    if (!video) {
+      console.log("VIDEO NÃO ENCONTRADO NESSA PÁGINA.");
+      return;
+    }
+
+    currentPage = Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS);
+    video.currentTime = (1 / FPS) * currentPage + 1 / (FPS * 2);
+
+    video.addEventListener("play", play);
+    video.addEventListener("pause", pause);
+    video.addEventListener("seeked", seeked);
+    document.querySelector("#next").addEventListener("click", nextPage);
+    document.querySelector("#previous").addEventListener("click", previousPage);
+
+    let cHeigth = video.clientHeight;
+    let cWidth = video.clientWidth;
+    let vHeigth = video.videoHeight;
+    let vWidth = video.videoWidth;
+
+    const canvas = GetCanvas("canvas");
+    const ctx = canvas.getContext("2d");
+
+    pause();
+
+    function GetCanvas(id) {
+      const canvas = document.createElement("canvas");
+      canvas.id = id;
+      var body = document.body, html = document.documentElement;
+
+      var Width = Math.max(
+        body.scrollWidth,
+        body.offsetWidth,
+        html.clientWidth,
+        html.scrollWidth,
+        html.offsetWidth
+      );
+      canvas.height = Width * PAGE_SCALE
+      canvas.width = (cWidth / cHeigth) * canvas.height;
+      canvas.style.left = `calc(50% - ${canvas.width / 2}px)`;
+      canvas.style.top = `calc((${canvas.width}px - ${canvas.height}px) / 2)`;
+      panelElement.appendChild(canvas);
+      return canvas;
+    }
+
+    function play() {
+      console.log("Play");
+      resizeCanvas()
+      video.play();
+      intervalID = setInterval(update, 1000 / 60.0);
+    }
+
+    function pause() {
+      console.log("Pause");
+      video.pause();
+
+      resizeCanvas()
+      DrawCanvas();
+
+      clearInterval(intervalID);
+    }
+
+    function nextPage() {
+      currentPage = Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS);
+      console.log(`Move to page: ${currentPage + 1}`);
+      video.currentTime = (1 / FPS) * (currentPage + 1) + 1 / (FPS * 2);
+      currentPage = Math.min(
+        Math.floor(video.getDuration() * FPS) - 1,
+        Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS)
+      );
+      DrawCanvas();
+    }
+
+    function previousPage() {
+      currentPage = Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS);
+      console.log(`Move to page: ${currentPage + 1}`);
+      video.currentTime = (1 / FPS) * (currentPage - 1) + 1 / (FPS * 2);
+      currentPage =  Math.max(
+        0,
+        Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS)
+      );
+      DrawCanvas();
+    }
+
+    function seeked() {
+      getCurrentPage();
+      pageViewer.textContent = `Page ${currentPage}`;
+      DrawCanvas();
+    }
+
+    function getCurrentPage() {
+      currentPage = Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS);
+      currentPage = Math.max(0, Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS));
+      currentPage = Math.min(Math.floor(video.getDuration() * FPS) - 1, Math.floor((video.getCurrentTime() - 1 / (FPS * 2)) * FPS));
+    }
+
+    function resizeCanvas(){
+      var body = document.body, html = document.documentElement;
+
+      var Width = Math.max(
+        body.scrollWidth,
+        body.offsetWidth,
+        html.clientWidth,
+        html.scrollWidth,
+        html.offsetWidth
+      );
+      canvas.height = Width * PAGE_SCALE
+      canvas.width = (cWidth / cHeigth) * canvas.height;
+      canvas.style.left = `calc(50% - ${canvas.width / 2}px)`;
+      canvas.style.top = `calc((${canvas.width}px - ${canvas.height}px) / 2)`;
+    }
+
+    function DrawCanvas() {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    }
+
+    function update() {
+      if (!(video.paused | video.ended)) {
+        DrawCanvas();
+        getCurrentPage()
+        pageViewer.textContent = `Page ${currentPage}`;
+      }
+    }
+  }
+};
